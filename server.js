103726d43e29537798dab15ca88a550ca4318ee5
Erik Smistad <ersmistad@gmail.com>
2017-02-01 12:48:19 +0100

    Small improvements to the chat

---
+++ server.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.username = false;
+    socket.on('chat message', function(msg){
+        console.log('msg received: ' + msg)
+        if(socket.username == false) { // Username not set yet
+            console.log('Setting username..')
+            socket.username = msg;
+            // Send only to this user
+            io.to(socket.id).emit('chat message', 'Welcome ' + socket.username + ' you can now chat.');
+
+            // Broadcast to everyone else that this user joined
+            socket.broadcast.emit('chat message', socket.username + ' joined the chat!');
+        } else {
+            // Send message to all
+            io.emit('chat message', socket.username + ': ' + msg);
+        }
+    });
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
