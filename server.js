85c2d0912e5fdc8053e10d6bb42101ac9c806392
Erik Smistad <ersmistad@gmail.com>
2017-02-01 14:08:29 +0100

    Put css in separate file

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/style.css', function(req, res){
+    res.sendFile(__dirname + '/style.css');
+});
 
 io.on('connection', function(socket){
     console.log('a user connected');
